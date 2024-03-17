1b8c106fe253430095d00daa5c4823e80e3e88e8
endijsbertans <endijsbertans@gmail.com>
2024-03-17 12:16:59 +0200

    Added Address class

---
+++ src/model/Parcel.java
@@ -0,0 +1,81 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Parcel {
+    private LocalDateTime orderCreated;
+    private LocalDateTime plannedDelivery;
+    private ParcelSize size;
+    private float price;
+    private boolean isFragile;
+    private Driver driver;
+    Parcel(){
+
+    }
+    Parcel(LocalDateTime orderCreated, LocalDateTime plannedDelivery, ParcelSize size, float price, boolean isFragile, Driver driver){
+
+    }
+
+    public LocalDateTime getOrderCreated() {
+        return orderCreated;
+    }
+
+    public void setOrderCreated() {
+        this.orderCreated = LocalDateTime.now();
+    }
+
+    public LocalDateTime getPlannedDelivery() {
+        return plannedDelivery;
+    }
+    // TODO  make a check for minimum delivery time 1 day
+    public void setPlannedDelivery(LocalDateTime plannedDelivery) {
+        if(plannedDelivery.isAfter(LocalDateTime.now()))
+            this.plannedDelivery = plannedDelivery;
+        else
+            this.plannedDelivery = LocalDateTime.now();
+    }
+
+    public ParcelSize getSize() {
+        return size;
+    }
+
+    public void setSize(ParcelSize size) {
+        this.size = size;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice() {
+        this.price = (float) ((size.getValue() * 1.99) + (isFragile ? 2.99 : 0));
+    }
+
+    public boolean isFragile() {
+        return isFragile;
+    }
+
+    public void setFragile(boolean fragile) {
+        isFragile = fragile;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    @Override
+    public String toString() {
+        return "Parcel{" +
+                "orderCreated=" + orderCreated +
+                ", plannedDelivery=" + plannedDelivery +
+                ", size=" + size +
+                ", price=" + price +
+                ", isFragile=" + isFragile +
+                ", driver=" + driver +
+                '}';
+    }
+}
--- src/model/ParcelSize.java
+++ src/model/ParcelSize.java
@@ -1,5 +1,15 @@
 package model;
 
 public enum ParcelSize {
-    X, S, M, L, XL
+    X(1), S(2), M(3), L(4), XL(5);
+
+    private final int value;
+
+    ParcelSize(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
