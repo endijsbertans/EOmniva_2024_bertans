7750d2830fdeb3464d7ac16aca667e1351698c5f
endijsbertans <endijsbertans@gmail.com>
2024-03-17 13:01:11 +0200

    Added AbstractCustomer class

---
+++ src/model/AbstractCustomer.java
@@ -0,0 +1,72 @@
+package model;
+
+import javax.xml.transform.stream.StreamSource;
+import java.util.ArrayList;
+
+public  abstract class AbstractCustomer{
+    private long cID;
+    private Address address;
+    private String phoneNo;
+    private String customerCode;
+    private ArrayList<Parcel> parcels = new ArrayList<Parcel>();
+    private static long counter = 0;
+    AbstractCustomer(){
+
+    }
+    AbstractCustomer(Address address, String phoneNo){
+
+    }
+
+    public long getcID() {
+        return cID;
+    }
+
+    public void setcID() {
+        this.cID = counter++;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        if(phoneNo != null &&
+                phoneNo.matches("2\\d{7}"))
+            this.phoneNo = phoneNo;
+        else
+            this.phoneNo = "2XXXXXXX";
+    }
+
+    public ArrayList<Parcel> getParcels() {
+        return parcels;
+    }
+
+    public void setParcels(ArrayList<Parcel> parcels) {
+        this.parcels = parcels;
+    }
+    public abstract void setCustomerCode(String customerCode);
+    public void addNewParcel(Parcel parcel){
+        if(!parcels.contains(parcel))
+            parcels.add(parcel);
+        System.out.println("Parcell already added!");
+    }
+
+    @Override
+    public String toString() {
+        return "AbstractCustomer{" +
+                "cID=" + cID +
+                ", address=" + address +
+                ", phoneNo='" + phoneNo + '\'' +
+                ", customerCode='" + customerCode + '\'' +
+                ", parcels=" + parcels +
+                '}';
+    }
+}
